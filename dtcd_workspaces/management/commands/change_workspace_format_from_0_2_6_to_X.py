10a64ea3eb7f2239000b2a0fd0bd135a021e74ab
Artem Zenkov <azenkov@isgneuro.com>
2023-11-03 16:31:24 +0700

    create keycloak recores in change_workspace_format script

--- dtcd_workspaces/management/commands/change_workspace_format_from_0_2_6_to_X.py
+++ dtcd_workspaces/management/commands/change_workspace_format_from_0_2_6_to_X.py
@@ -1,71 +1,108 @@
 import json
+import os
 import traceback
+import shutil
 
 from pathlib import Path
 from django.core.management.base import BaseCommand, CommandError
+
+from core.globals import global_vars
 from dtcd_workspaces.settings import WORKSPACE_BASE_PATH, DIR_META_NAME
 from dtcd_workspaces.workspaces.directory import Directory
 from dtcd_workspaces.workspaces.workspace import Workspace
+from dtcd_workspaces.workspaces.directory_content import DirectoryContent
 from dtcd_workspaces.workspaces.utils import encode_name, remove
 
-WORKSPACE_BASE_PATH = Path(WORKSPACE_BASE_PATH)
 
 
 class Command(BaseCommand):
     help = 'Update workspace and directory format'
 
-    def change_directory_format(self, dir_path: Path):
-        try:
-            with open(dir_path / DIR_META_NAME) as f:
-                directory = json.load(f)
-                dir_keys = set(directory.keys())
-                for key in dir_keys:
-                    if key not in Directory.saved_to_file_attributes:
-                        del directory[key]
-            for attr in Directory.saved_to_file_attributes:
-                if attr not in dir_keys:
-                    directory[attr] = None
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'old_format_workspace_dir',
+            help='Directory with workspaces, old format'
+        )
 
-            with open(dir_path / DIR_META_NAME, 'w') as f:
-                json.dump(directory, f)
+    def _get_new_path(self, old_format_filesystem_path: Path, name: str):
+        # for all directories get title and encode it
+        new_paths_dir_names = []
+        old_dir_names = str(old_format_filesystem_path.parent.relative_to(self.old_format_base_path)).split(os.sep)
+        if len(old_dir_names)==1 and old_dir_names[0] == '.':
+            new_path = name
+        else:
+            for i in range(len(old_dir_names)):
+                with open(self.old_format_base_path / os.sep.join(old_dir_names[0:i+1]) / DIR_META_NAME) as f:
+                    dir_dct = json.load(f)
+                    dir_name = dir_dct['title']
+                    new_paths_dir_names.append(dir_name)
+            new_path = os.sep.join(new_paths_dir_names) + os.sep + name
+        return new_path
 
-        except Exception as err:
-            self.stdout.write(self.style.ERROR(f'Error occured while change format for {dir_path}'))
-            self.stdout.write(self.style.ERROR(traceback.format_exc()))
+    def change_directory_format(self, old_format_dir_path: Path):
 
-        for child_item in dir_path.iterdir():
-            if child_item.is_dir():
+        self.stdout.write(self.style.WARNING(f'Handle path {old_format_dir_path}'))
+        if old_format_dir_path!=self.old_format_base_path:
+            try:
+                with open(old_format_dir_path / DIR_META_NAME, 'r') as f:
+                    directory_dict = json.load(f)
+                    title = directory_dict.get('title', '')
+                    dir_keys = set(directory_dict.keys())
+                    for key in dir_keys:
+                        if key not in Directory.saved_to_file_attributes:
+                            del directory_dict[key]
+                for attr in Directory.saved_to_file_attributes:
+                    if attr not in dir_keys:
+                        directory_dict[attr] = None
+
+                dir_path = self._get_new_path(old_format_dir_path, title)
+                self.stdout.write(self.style.WARNING(f'Create directory path={dir_path}'))
+                Directory.create(
+                    str(dir_path),
+                    **directory_dict
+                )
+
+            except Exception as err:
+                self.stdout.write(self.style.ERROR(f'Error occured while change format for {old_format_dir_path}'))
+                self.stdout.write(self.style.ERROR(traceback.format_exc()))
+
+        for child_item in old_format_dir_path.iterdir():
+            if child_item.is_dir() and (child_item / DIR_META_NAME).exists():
                 self.change_directory_format(child_item)
             else:
                 if child_item.name.endswith('.json'):
                     self.change_workspace_format(child_item)
 
-    def change_workspace_format(self, workspace_path: Path):
-        # rename workspace with title, remove id
+    def change_workspace_format(self, old_format_workspace_path: Path):
+        self.stdout.write(self.style.WARNING(f'Handle path {old_format_workspace_path}'))
         try:
-            with open(workspace_path) as f:
-                workspace = json.load(f)
-                title = workspace['title']
-                w_keys = set(workspace.keys())
+            with open(old_format_workspace_path) as f:
+                workspace_dict = json.load(f)
+                title = workspace_dict['title']
+                w_keys = set(workspace_dict.keys())
                 for key in w_keys:
                     if key not in Workspace.saved_to_file_attributes:
-                        del workspace[key]
+                        del workspace_dict[key]
                 for attr in Workspace.saved_to_file_attributes:
                     if attr not in w_keys:
-                        workspace[attr] = None
-                new_workspace_path = workspace_path.parent / encode_name(title)
-                with open(new_workspace_path, 'w') as f_out:
-                    json.dump(workspace, f_out)
-                    remove(workspace_path)
+                        workspace_dict[attr] = None
+                new_workspace_path = self._get_new_path(old_format_workspace_path, title)
+                self.stdout.write(self.style.WARNING(f'Create workspace path={new_workspace_path}'))
+                workspace = Workspace.create(
+                    str(new_workspace_path),
+                    **workspace_dict
+                )
 
         except Exception as err:
-            self.stdout.write(self.style.ERROR(f'Invalid workspace format {workspace_path}'))
+            self.stdout.write(self.style.ERROR(f'Invalid workspace format {old_format_workspace_path}'))
             self.stdout.write(self.style.ERROR(traceback.format_exc()))
 
     def handle(self, *args, **options):
-
-        base_path = Path(WORKSPACE_BASE_PATH)
-        self.change_directory_format(base_path)
-
+        global_vars['disable_authorization'] = True
+        self.old_format_base_path = Path(options['old_format_workspace_dir'])
+        self.base_path = Path(WORKSPACE_BASE_PATH)
+        # copy root DIR_INFO
+        shutil.copy(self.old_format_base_path / DIR_META_NAME, Path(WORKSPACE_BASE_PATH) / DIR_META_NAME)
+        self.change_directory_format(self.old_format_base_path)
         self.stdout.write(self.style.SUCCESS('Successfully changed format'))
 
